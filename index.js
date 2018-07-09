3248b5b0c2a62e8a6a568ac80635e2dac5d5489d
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-07-09 20:10:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const app = "I don't do much.";
 
+const token = 'c314f080dc36762760a60b90d35f0cf83519c6da'
+
 fetch('https://api.github.com/user/repos').
   then(res => res.json()).
   then(json => console.log(json))
 
-//c314f080dc36762760a60b90d35f0cf83519c6da
+//
