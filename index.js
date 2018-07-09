56b529b7d49f745c0a937619dc01e90da4408ebe
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-07-09 20:11:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const app = "I don't do much.";
+const token = 'c314f080dc36762760a60b90d35f0cf83519c6da'
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
