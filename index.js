54e4898051c6c72f21fea5463a40cfa733176a72
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-07-09 20:10:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const token = 'c314f080dc36762760a60b90d35f0cf83519c6da'
 
-fetch('https://api.github.com/user/repos').
-  then(res => res.json()).
-  then(json => console.log(json))
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
