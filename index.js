07c610bbcbef10af957ab774dc5e901bd4c3d82e
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-07-09 20:09:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = "I don't do much.";
 
-fetch('https://api.github.com/repos/jquery/jquery/commits')
-  .then(res => res.json())
-  .then(json => console.log(json));
+fetch('https://api.github.com/user/repos').
+  then(res => res.json()).
+  then(json => console.log(json))
 
+//c314f080dc36762760a60b90d35f0cf83519c6da
